b5d2ca254bf5b1e4073b9a66ab650578a80bdc49
Chouser <chouser@n01se.net>
2011-08-16 09:25:09 -0400

    First potentially complete talk outline

--- config.cljs
+++ config.cljs
@@ -3,4 +3,19 @@
   [:view-title {:once/title {:opacity 1}}
    :view-main {}
    :view-read {}
-   :view-eval {}]}
+   :view-eval {}
+   :view-eval-detail {}
+   :view-analyze {}
+   :view-pojos {}
+   :view-analyze {}
+   :view-nested-maps {}
+   :view-analyze {}
+   :view-emit-in {}
+   :view-emit-out {}
+   :view-load {}
+   :view-eval {}
+   :view-print {}
+   :view-print-detail {}
+   :view-loop {}
+   :view-main {}
+   ]}
